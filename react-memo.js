76a6e7d4a18fb765207265335c80a7044cec7ab8
Bushra Aboubida <58691127+dev-bushra@users.noreply.github.com>
2022-06-28 21:47:11 +0400

    Update react-memo.js

--- react-memo.js
+++ react-memo.js
@@ -1,2 +1,11 @@
-// title
-const content = 0;
+// # React Hooks:
+// 1. useState: Declare a state in a function component.
+const [count, setCount] = useState(3);
+const [counter, setCounter] = useState(() => { return 3 }) // state value only run once (When the component first mounted).
+// 2. useEffect
+useEffect(() => { console.log("I run everytime this component rerenders") }) // Every rerender
+useEffect(() => { console.log("I Only run once (When the component gets mounted)") }, []) // onMount
+useEffect(() => { console.log("I run everytime my condition is changed") }, [condition]) // Condition based 
+useEffect(() => { return () => { console.log("Use this return as a 'clean up tool' (this runs when unmounting and before the actual code)") } }); // Clean up
+// 3. useMemo: will only recompute the memoized value when one of the dependencies has changed. This optimization helps to avoid expensive calculations on every render.
+const memoizedValue = useMemo(() => { computeExpensiveValue(a, b) }, [a, b]);
